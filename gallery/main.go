5c8cb738e759756e8f6f7ef3f1e9b6a95b375439
UponTheSky <sailing.to.orbis@gmail.com>
2024-04-04 08:41:57 +0900

    feat: gracefull shutdown

--- gallery/main.go
+++ gallery/main.go
@@ -7,22 +7,24 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	execLogger := logger.NewSLogger(os.Stdout, slog.LevelDebug)
+	logCtx := context.Background()
 	srv := setupServer()
 
-	sysLogger := logger.NewSLogger(os.Stdout, slog.LevelDebug)
-	ctx := context.Background()
-
-	sysLogger.LogAttrs(
-		ctx,
+	execLogger.LogAttrs(
+		logCtx,
 		slog.LevelInfo,
 		"The server starts running at",
 		slog.String("address", srv.Addr),
 	)
 
-	sysLogger.LogAttrs(ctx, slog.LevelError, srv.ListenAndServe().Error())
+	execLogger.Info(runServer(logCtx, execLogger, srv).Error())
 	os.Exit(1)
 }
 
@@ -40,4 +42,57 @@ func setupServer() *http.Server {
 	)
 
 	return server.New(srvConfig)
+}
+
+// graceful shutdown
+// see the example: https://pkg.go.dev/net/http#Server.Shutdown
+func runServer(logCtx context.Context, logger *slog.Logger, srv *http.Server) error {
+	// to wait for the goroutine to finish before the main thread
+	idleConnsClosed := make(chan struct{})
+
+	go func() {
+		// waiting for the interrupt signal to come
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-sigChan
+
+		// catches sigint now
+		logger.LogAttrs(
+			logCtx,
+			slog.LevelInfo,
+			"server shutdown by an external signal",
+			slog.String("signal", sig.String()),
+		)
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			logger.LogAttrs(
+				logCtx,
+				slog.LevelError,
+				"The HTTP server shutdown error",
+				slog.String("error", err.Error()),
+			)
+		}
+
+		// tell the main thread that this goroutine has finished
+		close(idleConnsClosed)
+	}()
+
+	err := srv.ListenAndServe()
+
+	if err != http.ErrServerClosed {
+		logger.LogAttrs(
+			logCtx,
+			slog.LevelError,
+			"The HTTP server error",
+			slog.String("error", err.Error()),
+		)
+		panic(err)
+	}
+
+	<-idleConnsClosed
+
+	return err
 }
