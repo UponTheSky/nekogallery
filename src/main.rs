869d821fbbe03100e61396c74846f8be374486a3
UponTheSky <sailing.to.orbis@gmail.com>
2024-12-19 00:28:06 +0900

    ex1: write an echo server in axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use axum::{
+    extract::{Path, Query},
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = make_app();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn make_app() -> Router {
+    let router = Router::new();
+
+    router.route("/:string", get(echo))
+}
+
+async fn echo(Path(string): Path<String>) -> String {
+    println!("get the word {}", &string);
+    string
 }
